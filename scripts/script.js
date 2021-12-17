8de3f8dcd03d30284ce03e02704782eb54ff3fea
gcerv93 <gcerv93@protonmail.com>
2021-12-17 09:39:12 -0600

    working hover effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 const container = document.querySelector('.container');
 
+createGrid(16);
+
 function createGrid(size) {
   i = 1
+  // create rows
   while (i <= size) {
     const row = document.createElement('div');
     row.classList.toggle('row');
@@ -11,6 +14,7 @@ function createGrid(size) {
   }
 
   const rows = document.querySelectorAll(".row");
+  // create cells for each row
   rows.forEach(row => {
     i = 1
     while (i <= size) {
@@ -23,4 +27,11 @@ function createGrid(size) {
   })
 }
 
-createGrid(16);
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach(cell => {
+  cell.addEventListener('mouseover', (e) => {
+    // when a specific cell is targeted, change its color
+    e.target.setAttribute("style",  "background-color: black");
+  })
+})
